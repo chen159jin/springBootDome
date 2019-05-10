d09fc484d652a07687f1a4bbf52d97c0daf0c9c1
Jin <123@163.com>
2019-05-10 16:38:49 +0800

    创建项目

--- demoSecurity/src/main/java/com/jin/demo/Application.java
+++ demoSecurity/src/main/java/com/jin/demo/Application.java
@@ -1,8 +1,9 @@
 package com.jin.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan({"com.**.dao"})
 @SpringBootApplication
 public class Application {
 
